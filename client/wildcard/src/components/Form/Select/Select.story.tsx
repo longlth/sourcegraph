69c1c09a426ee9f283a8347f1e9c19a2a8ab3279
gitstart-sourcegraph <gitstart@users.noreply.github.com>
2022-01-19 15:17:29 +0000

    refactor: added CSS rule

--- client/wildcard/src/components/Form/Select/Select.story.tsx
+++ client/wildcard/src/components/Form/Select/Select.story.tsx
@@ -7,6 +7,7 @@ import webStyles from '@sourcegraph/web/src/SourcegraphWebApp.scss'
 import { Grid } from '../../Grid/Grid'
 
 import { Select, SelectProps } from './Select'
+import styles from './story.module.scss';
 
 const config: Meta = {
     title: 'wildcard/Select',
@@ -56,7 +57,7 @@ const BaseSelect = (props: { id: string } & Pick<SelectProps, 'isCustomStyle' | 
 const SelectVariants = ({ isCustomStyle }: Pick<SelectProps, 'isCustomStyle'>) => {
     const idPrefix = isCustomStyle ? 'custom' : 'native'
     return (
-        <Grid columnCount={4}>
+        <Grid className={styles.story} columnCount={4}>
             <div>
                 <h2>Standard</h2>
                 <BaseSelect id={`${idPrefix}-standard`} isCustomStyle={isCustomStyle} />
