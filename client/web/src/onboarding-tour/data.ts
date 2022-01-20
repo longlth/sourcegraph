c9199ba8debe44fda8595906229c3f97c9558d6d
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2022-01-20 22:42:28 +0600

    Add "languages" support + refactoring

--- client/web/src/onboarding-tour/data.ts
+++ client/web/src/onboarding-tour/data.ts
@@ -1,3 +1,5 @@
+import { OnboardingTourLanguage } from '../stores/onboardingTourState'
+
 const CODE_SEARCH = 'Code search use cases'
 const CODE_INTEL = 'The power of code intel'
 const TOOLS = 'Tools to improve workflow'
@@ -15,7 +17,7 @@ export interface OnboardingTourStepItem {
     /**
      * URL to redirect
      */
-    to: string
+    url: string | Record<OnboardingTourLanguage, string>
     /**
      * Flag whether this step was completed of not
      */
@@ -36,7 +38,7 @@ export const ONBOARDING_STEP_ITEMS: Omit<OnboardingTourStepItem, 'isCompleted'>[
         id: 'TourSymbolsSearch',
         title: 'Search multiple repos',
         group: CODE_SEARCH,
-        to: '/search?q=context:global+repo:linkedin+lang:java+AtomicBoolean&patternType=literal',
+        url: '/search?q=context:global+repo:linkedin+lang:java+AtomicBoolean&patternType=literal',
         info: `<strong>Reference code in multiple repositories</strong><br/>
             The repo: query allows searching in multiple repositories matching a term. Use it to reference all of your projects or find open source examples.`,
     },
@@ -44,7 +46,7 @@ export const ONBOARDING_STEP_ITEMS: Omit<OnboardingTourStepItem, 'isCompleted'>[
         id: 'TourCommitsSearch',
         title: 'Find changes in commits',
         group: CODE_SEARCH,
-        to:
+        url:
             '/search?q=context:global+repo:%5Egitlab%5C.com/sourcegraph/sourcegraph%24+type:commit+bump&patternType=literal',
         info: `<strong>Find changes in commits</strong><br/>
             Quickly find commits in history, then browse code from the commit, without checking out the branch.`,
@@ -53,8 +55,22 @@ export const ONBOARDING_STEP_ITEMS: Omit<OnboardingTourStepItem, 'isCompleted'>[
         id: 'TourDiffSearch',
         title: 'Search diffs for added code',
         group: CODE_SEARCH,
-        to:
-            '/search?q=context:global+repo:sourcegraph-testing/+lang:java+type:diff+select:commit.diff.added+//TODO&patternType=literal',
+        url: {
+            [OnboardingTourLanguage.C]:
+                '/search?q=context:global+repo:curl/doh+type:diff+select:commit.diff.removed+mode&patternType=literal',
+            [OnboardingTourLanguage.Go]:
+                '/search?q=context:global+repo:%5Egitlab%5C.com/sourcegraph/sourcegraph%24+type:diff+lang:go+select:commit.diff.removed+NameSpaceOrgId&patternType=literal',
+            [OnboardingTourLanguage.Java]:
+                '/search?q=context:global+repo:sourcegraph-testing/sg-hadoop+lang:java+type:diff+select:commit.diff.removed+getConf&patternType=literal',
+            [OnboardingTourLanguage.Javascript]:
+                '/search?q=context:global+repo:sourcegraph/sourcegraph%24+lang:javascript+-file:test+type:diff+select:commit.diff.removed+promise&patternType=literal',
+            [OnboardingTourLanguage.Php]:
+                '/search?q=context:global+repo:laravel/laravel.*+lang:php++type:diff+select:commit.diff.removed+password&patternType=regexp&case=yes',
+            [OnboardingTourLanguage.Python]:
+                '/search?q=context:global+repo:pallets/+lang:python+type:diff+select:commit.diff.removed+password&patternType=regexp&case=yes',
+            [OnboardingTourLanguage.Typescript]:
+                '/search?q=context:global+repo:sourcegraph/sourcegraph%24+lang:typescript+type:diff+select:commit.diff.removed+authenticatedUser&patternType=regexp&case=yes',
+        },
         info: `<strong>Searching diffs for added code</strong><br/>
             Find altered code without browsing through history or trying to remember which file it was in.`,
     },
@@ -66,7 +82,7 @@ export const ONBOARDING_STEP_ITEMS: Omit<OnboardingTourStepItem, 'isCompleted'>[
         info: `<strong>FIND REFERENCES</strong><br/>
             Hover over a token in the highlighted line to open code intel, then click ‘Find References’ to locate all calls of this code.`,
         completeAfterEvents: ['findReferences'],
-        to: '/github.com/sourcegraph/sourcegraph/-/blob/internal/featureflag/featureflag.go?L9:6',
+        url: '/github.com/sourcegraph/sourcegraph/-/blob/internal/featureflag/featureflag.go?L9:6',
     },
     {
         id: 'TourGoToDefinition',
@@ -75,19 +91,19 @@ export const ONBOARDING_STEP_ITEMS: Omit<OnboardingTourStepItem, 'isCompleted'>[
         info: `<strong>GO TO DEFINITION</strong><br/>
             Hover over a token in the highlighted line to open code intel, then click ‘Go to definition’ to locate a token definition.`,
         completeAfterEvents: ['goToDefinition', 'goToDefinition.preloaded'],
-        to: '/github.com/sourcegraph/sourcegraph/-/blob/internal/repos/observability.go?L192:22',
+        url: '/github.com/sourcegraph/sourcegraph/-/blob/internal/repos/observability.go?L192:22',
     },
     // Group: TOOLS
     {
         id: 'TourEditorExtensions',
         group: TOOLS,
         title: 'IDE extensions',
-        to: 'https://docs.sourcegraph.com/integration/editor',
+        url: 'https://docs.sourcegraph.com/integration/editor',
     },
     {
         id: 'TourBrowserExtensions',
         group: TOOLS,
         title: 'Browser extensions',
-        to: 'https://docs.sourcegraph.com/integration/browser_extension',
+        url: 'https://docs.sourcegraph.com/integration/browser_extension',
     },
 ]
